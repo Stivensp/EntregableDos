b7d913769bfd08caabda8d95d8ed8b6d7fa1e436
Stiven <delleore1@gmail.com>
2023-11-27 23:44:22 -0500

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header =({Header})=>{
      <h3 className="Weather">Weather App</h3>
      <form action="/buscar">
     <input type="text" placeholder="Buscar" className="search"></input>       
-    <img className="lupa" src="\public\img\icons\material-symbols_search.png" alt="" />
+    <img className="lupa" src="\img\icons\material-symbols_search.png" alt="" />
         </form>   
     <button className="bt__search"></button>
 
